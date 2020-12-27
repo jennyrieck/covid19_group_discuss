3c4962c724ebf77214ef8444a19092ce4df4e3bf
jennyrieck <jenny.rieck@gmail.com>
2020-12-27 17:58:27 -0500

    dataset create

--- R/0_prepText_countWords.R
+++ R/0_prepText_countWords.R
@@ -8,10 +8,38 @@ library(textnets)
 download.file("https://ckan0.cf.opendata.inter.prod-toronto.ca/download_resource/55829124-3d8d-4dfa-9102-40911837e6d7", 
               "../data/spt-group-discussion-results.csv")
 
-data.in<-read.csv('spt-group-discussion-results.csv', na.strings = c('N/A', '', 'n/a','na'), stringsAsFactors = F)
+data.in<-read.csv('../data/spt-group-discussion-results.csv', na.strings = c('N/A', '', 'n/a','na'), stringsAsFactors = F)
+
+
+######################################################
+### Formant Vulnerable Population Demographic Characteristics
+######################################################
+
+demog.vars.cols<-c(4:20)
+demogs.counts.out<-data.in[,demog.vars.cols]
+demogs.counts.out[is.na(demogs.counts.out)]<-0
+
+## Shorten demographic group labels 
+demogs.labels<-names(demogs.counts.out)
+demogs.labels<-gsub('Persons.*ing.','',demogs.labels)
+demogs.labels<-gsub('People.with.','',demogs.labels)
+demogs.labels<-gsub('P.*face.','',demogs.labels)
+demogs.labels<-gsub('.based','',demogs.labels)
+demogs.labels<-substr(demogs.labels,1,10)
+
+## Create focus group by demographic counts matrix
+colnames(demogs.counts.out)<-toupper(demogs.labels)
+rownames(demogs.counts.out)<-data.in$Respondent.ID
+
+
+######################################################
+### Formant word usage in group discussion narratives
+######################################################
 
 ## Specify which question to analyze
 grpdisc.Q <-'Ways.in.which.you.were.personally.impacted'
+#grpdisc.Q <-'Supports..programs.and.services.you.were.accessing.before.COVID.19'
+#grpdisc.Q <- 'Urgent.supports.needed'
 
 ## Count lemmas in narrative text, removing common stop words (e.g., "the", "is", "that")
 grpdisc.word.counts.all<- PrepText(textdata = data.in,groupvar =  'Respondent.ID', textvar = grpdisc.Q,
@@ -27,10 +55,9 @@ words.to.keep<- words.to.keep[(nchar(words.to.keep) >= 3)]
 ## Remove words that don't begin with a letter (i.e., numbers)
 words.to.keep<-grep('^[[:alpha:]]',words.to.keep,value = T)
 
-## Other common words to remove that are not as descriptive
+## Other common words to remove
 other.words.to.remove<-c("none", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                          "either", "whether", "thing", "something", "also", "moreover", "since")
-
 words.to.keep<-words.to.keep[!words.to.keep %in% other.words.to.remove]
 
 ### reformat data with only the lemma we want
@@ -42,124 +69,18 @@ for(w in words.to.keep){
 
 grpdisc.word.counts.cut<-grpdisc.word.counts.all[grpdisc.word.counts.all$keep==T,]
 
-
-### Reshape the words counts so it's ids by words (counts by focus group)
+### Reshape the words counts so it's focus group ids by word counts
 grpdisc.word.counts.by.id<-as.data.frame.matrix(table(grpdisc.word.counts.cut$Respondent.ID, grpdisc.word.counts.cut$lemma))
 
-## Run Correspondence Analyasis (CA) on the word count data
-words.ca.res<-epCA(grpdisc.word.counts.by.id,graphs=F)
 
-################################
-### Demographic variables
+###############################################
+###### Final format and save both count matrices
 
-demog.vars.cols<-c(4:20)
-demogs.counts.out<-data.in[,demog.vars.cols]
-demogs.counts.out[is.na(demogs.counts.out)]<-0
+### Some focus groups did not provide answers for all questions, 
+### so we must find the groups common between our demographics an dour word counts
 
-## Shorten demographic group labels 
-demogs.labels<-names(demogs.counts.out)
-demogs.labels<-gsub('Persons.*ing.','',demogs.labels)
-demogs.labels<-gsub('People.with.','',demogs.labels)
-demogs.labels<-gsub('P.*face.','',demogs.labels)
-demogs.labels<-gsub('.based','',demogs.labels)
-demogs.labels<-substr(demogs.labels,1,10)
+X.demogs<-as.matrix(demogs.counts.out[match( rownames(grpdisc.word.counts.by.id),rownames(demogs.counts.out)),])
+Y.words<-as.matrix(grpdisc.word.counts.by.id)
 
-colnames(demogs.counts.out)<-toupper(demogs.labels)
-
-rownames(demogs.counts.out)<-data.in$Respondent.ID
-
-## Run CA on the demographic charateristics counts
-demog.ca.res<-epCA(demogs.counts.out, graphs=F)
-
-#tog.words.demogs<-cbind(grpdisc.word.counts.by.id,
-#                        demogs.counts.out[match( rownames(grpdisc.word.counts.by.id),rownames(demogs.counts.out)),])
-
-#epCA(tog.words.demogs,graphs = F)
-
-
-## Run PLS-CA to examine the joint relationship between different demographic variables and words used in their narrative
-X<-as.matrix(demogs.counts.out[match( rownames(grpdisc.word.counts.by.id),rownames(demogs.counts.out)),])
-Y<-as.matrix(grpdisc.word.counts.by.id)
-
-plsca.res<-plsca_cor(X = X, Y = Y)
-taus <- plsca.res$l_full/sum(plsca.res$l_full)
-
-ax1<-1
-ax2<-2
-plot.constraints<-minmaxHelper(plsca.res$fi, plsca.res$fj, axis1 = ax1, axis2=ax2)
-#plot.constraints<-minmaxHelper(plsca.res$p, plsca.res$q, axis1 = ax1, axis2=ax2)
-
-# windows()
-# prettyPlot(plsca.res$fi)
-# windows()
-# prettyPlot(plsca.res$fj)
-
-## Which components to plot
-
-
-# prettyPlot(plsca.res$fi,x_axis = ax1, y_axis=ax2,constraints = plot.constraints,
-#            contributionCircles = T, contributions = plsca.res$u^2 )
-# 
-# prettyPlot(plsca.res$fj,x_axis = ax1, y_axis=ax2,constraints = plot.constraints,
-#            contributionCircles = T, contributions = plsca.res$e^2 )
-# 
-
-library(ggplot2)
-library(ggrepel)
-
-plot.fi<-ggplot(as.data.frame(plsca.res$fi[,c(ax1,ax2)]),
-                aes(V1, V2,label=rownames(plsca.res$fi))) + 
-  geom_point(color = "navy",size=5, pch=18) + geom_point(color = "white",size=2, pch=18) +
-  xlim(c(plot.constraints$minx, plot.constraints$maxx)) + ylim(c(plot.constraints$miny, plot.constraints$maxy)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        axis.text.x=element_blank(),  axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank()) +
-  geom_vline(xintercept = 0,color='darkgray') + geom_hline(yintercept = 0,color='darkgray') +
-  geom_label_repel(size=3.5, box.padding = 0.25, point.padding = 0.25, direction = "both",
-                   min.segment.length = 0.3 ) +
-  xlab(paste0("Component 1. Explained variance: ", round(taus[ax1]*100, digits=1),"%")) +
-  ylab(paste0("Component 2. Explained variance: ", round(taus[ax2]*100, digits=1),"%")) +
-  ggtitle(paste0(grpdisc.Q, "\nCA: Group Demographics Component Scores")) +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=16))
-        
-
-### Select which words to plot (top 25 words per component)
-
-#contribs<-(plsca.res$v[,c(ax1, ax2)])^2
-#expected.contrib<-1/(dim(plsca.res$v)[1])*2.5
-#extreme.words<-rowSums(contribs>expected.contrib)
-#extreme.words.labels<-rownames(plsca.res$fj)  
-#extreme.words.labels[extreme.words==0]<-""
-
-top.n<-30
-ax1.top<-names(sort(abs(plsca.res$fj[,ax1]),decreasing = T))[1:top.n]
-ax2.top<- names(sort(abs(plsca.res$fj[,ax2]),decreasing = T))[1:top.n]
-all.top<-unique(c(ax1.top, ax2.top))
-
-extreme.words.labels<-rownames(plsca.res$fj) 
-extreme.words.labels[!(extreme.words.labels %in% all.top)]<-""
-
-
-plot.fj<-ggplot(as.data.frame(plsca.res$fj[,c(ax1,ax2)]),
-                aes(V1, V2, label=extreme.words.labels)) + 
-  geom_point(color = "gray36",size=2) +
-  xlim(c(plot.constraints$minx, plot.constraints$maxx)) + ylim(c(plot.constraints$miny, plot.constraints$maxy)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        axis.text.x=element_blank(),  axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank()) +
-  geom_vline(xintercept = 0,color='darkgray') + geom_hline(yintercept = 0,color='darkgray') +
-  geom_label_repel(size=2.5, box.padding = 0.25, point.padding = 0.25, direction = "both",
-                   min.segment.length = 0.3 ) +
-  xlab(paste0("Component 1. Explained variance: ", round(taus[ax1]*100, digits=1),"%")) +
-  ylab(paste0("Component 2. Explained variance: ", round(taus[ax2]*100, digits=1),"%")) +
-  ggtitle(paste0(grpdisc.Q, "\nCA: Group Demographics Component Scores")) +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=16))
-
-
-
-
+save(grpdisc.Q, X.demogs, Y.words, file=paste0('../data/word_counts_', substr(grpdisc.Q,1,42), '.rda'))
 
