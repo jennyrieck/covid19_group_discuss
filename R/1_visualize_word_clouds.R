90c0f6d26b04e78229529d98a03e1884eaf3ae68
jennyrieck <jenny.rieck@gmail.com>
2020-12-28 11:43:18 -0500

    title plotting
    and freqvar fix

--- R/1_visualize_word_clouds.R
+++ R/1_visualize_word_clouds.R
@@ -27,24 +27,36 @@ layout(matrix(c(1, 2), nrow=2), heights=c(.25, 4))
 par(mar=rep(0, 4))
 plot.new()
 text(x=0.5, y=.5, 'Vulnerable Populations Demographics',cex = 1.5)
-wordcloud(words = full.demog.cloud.mat$word, freq = full.word.cloud.mat$freq, min.freq = 1,           
-          max.words=500, random.order=FALSE, rot.per=0, scale=c(4.5,1),
+wordcloud(words = full.demog.cloud.mat$word, freq = full.demog.cloud.mat$freq, min.freq = 1,           
+          max.words=500, random.order=FALSE, rot.per=0, scale=c(4.25,1),
           colors=brewer.pal(8, "Dark2"))
 dev.off()
 
 ###############################
 ### Word frequency cloud
 ###############################
+## force a new paragraph for title after 90 characters
+##gsub('(.{1,90})(\\s|$)', '\\1\n', s)
 
 full.word.cloud.mat<-data.frame(word = colnames(Y.words),freq=colSums(Y.words))
 set.seed(1234) # for reproducibility 
 
+### If title is longer than 36 characters, it will word wrap onto next line for title
+if(nchar(grpdisc.Q>36)){
+  plot.title<- paste0(gsub('\\.', ' ', substr(grpdisc.Q,1,36)),'\n',gsub('\\.', ' ', substr(grpdisc.Q,37,64)))
+  title.margin<-.5
+}else{
+  plot.title <-gsub('\\.', ' ', substr(grpdisc.Q,1,36))
+  title.margin<-.25
+}
+
 png(file=paste0("../plots/wordcloud_words_",substr(grpdisc.Q,1,42),".png"),height=400,width=400)
-layout(matrix(c(1, 2), nrow=2), heights=c(.25, 4))
+layout(matrix(c(1, 2), nrow=2), heights=c(title.margin, 4))
 par(mar=rep(0, 4))
 plot.new()
-text(x=0.5, y=0.5, gsub('\\.', ' ', grpdisc.Q),cex = 1.5)
+
+text(x=0.5, y=0.5, plot.title,cex = 1.5)
 wordcloud(words = full.word.cloud.mat$word, freq = full.word.cloud.mat$freq, min.freq = 4,           
-          max.words=300, random.order=FALSE, rot.per=0, scale=c(3.5,0.25),           
+          max.words=300, random.order=FALSE, rot.per=0, scale=c(3,0.25),           
           colors=brewer.pal(8, "Dark2"))
 dev.off()
